97b2f1cb3ba6b68735f5642f689f7d2fc7e14240
tengbin <tengbin@boqii.com>
2016-03-15 15:28:12 +0800

    add markdown

--- app/src/main/java/com/mytian/lb/fragment/KindleFragment.java
+++ app/src/main/java/com/mytian/lb/fragment/KindleFragment.java
@@ -2,8 +2,14 @@ package com.mytian.lb.fragment;
 
 import com.mytian.lb.AbsFragment;
 import com.mytian.lb.R;
+import com.mytian.lb.mview.MarkDownView;
+
+import butterknife.Bind;
 
 public class KindleFragment extends AbsFragment {
+
+    @Bind(R.id.markdownview)
+    MarkDownView markDownView;
 
     @Override
     public boolean onBackPressed() {
@@ -18,5 +24,8 @@ public class KindleFragment extends AbsFragment {
     @Override
     public void EInit() {
         super.EInit();
+        markDownView.setStringSource("#aibao\n" +
+                "\n" +
+                "[download](http://fir.im/5xv4)");
     }
 }
